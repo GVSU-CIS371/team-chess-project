cafc098d9b269c2bdd743126fb3bb0dc2642bf2f
Sam <samhanslits@gmail.com>
2025-12-01 21:01:49 -0500

    Incorporated chessboard.js into the project

--- my-vue-app/src/views/OpeningView.vue
+++ my-vue-app/src/views/OpeningView.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { computed, onMounted, ref } from 'vue'
+
+declare global {
+  interface Window { Chessboard: any; }
+}
+
+const route = useRoute()
+const openingId = computed(() => route.params.id)
+
+const boardEl = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  if (boardEl.value) {
+    const board = new window.Chessboard(boardEl.value, {
+      position: 'start',
+      pieceTheme: '/img/chesspieces/wikipedia/{piece}.png'
+    })
+  }
+})
 
 </script>
 
 <template>
   <div>
-    <h1>Opening Page</h1>
+    <h1>{{ openingId }}</h1>
+    <div ref="boardEl" style="width: 400px; margin: 1rem auto;"></div>
   </div>
 </template>
