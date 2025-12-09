40a39332b1eaba5fe2d38d3bb5fe228c4b250aa3
Sam <samhanslits@gmail.com>
2025-12-09 00:35:00 -0500

    Paired the app with the firestore

--- openings_teacher/src/firebase.ts
+++ openings_teacher/src/firebase.ts
@@ -2,14 +2,14 @@ import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyAMHJKh_yfgX0VxhFWXTAoJtB6V9OTU3II",
-  authDomain: "cis371-a2af1.firebaseapp.com",
-  projectId: "cis371-a2af1",
-  storageBucket: "cis371-a2af1.firebasestorage.app",
-  messagingSenderId: "2471886198",
-  appId: "1:2471886198:web:9f820b8322b90658750e57"
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAWHlSGuUl7jMJHum1x01m9pmLi5fGqcjk",
+    authDomain: "chess-openings-teacher.firebaseapp.com",
+    projectId: "chess-openings-teacher",
+    storageBucket: "chess-openings-teacher.firebasestorage.app",
+    messagingSenderId: "827252316524",
+    appId: "1:827252316524:web:6a3394e6b20a1b54b4fb49"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
